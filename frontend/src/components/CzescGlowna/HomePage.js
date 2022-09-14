a2263bf329762be29d1b6b756ba8e8c6999e3ac3
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-09-14 12:11:10 +0200

    Reaneme StronaGłowna.js to HomePage.js and change content

---
+++ frontend/src/components/CzescGlowna/HomePage.js
@@ -0,0 +1,32 @@
+import Header from "./Header";
+import "./HomePage.css";
+
+function HomePage() {
+    return (
+        <div className="App">
+            <Header></Header>
+            <main className="main">
+                <div className="main__content">
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                    <div className="main__content-item"></div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
