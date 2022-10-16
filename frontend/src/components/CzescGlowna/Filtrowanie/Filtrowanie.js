f7df1e68b9b13564d5bf8146957301ee27d8f60a
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-10-16 19:18:19 +0200

    Add component Filtrowanie

---
+++ frontend/src/components/CzescGlowna/Filtrowanie/Filtrowanie.js
@@ -0,0 +1,16 @@
+import Header from "../Header/Header";
+import "./Filtrowanie.css";
+
+function Filtrowanie() {
+    return (
+        <div className="App">
+            <Header></Header>
+            <main className="main">
+                <div className="main__content">
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Filtrowanie;
