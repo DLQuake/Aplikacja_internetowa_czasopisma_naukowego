5964ddc38f1a67263907b83c67a8ccab29464142
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-10-23 20:06:48 +0200

    Create test routes, modify app.js in backend folder, modify HomePage component in frontend and test connect backend with frontend

--- frontend/src/components/CzescGlowna/HomePage/HomePage.js
+++ frontend/src/components/CzescGlowna/HomePage/HomePage.js
@@ -1,33 +1,52 @@
+import { Component } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import "./HomePage.css";
 
-function HomePage() {
-    return (
-        <div className="App">
-            <Header></Header>
-            <main className="c-HomePage-main">
-                <div className="c-HomePage-main__content">
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                    <Link className="c-HomePage-main__content-item" to="#"></Link>
-                </div>
-            </main>
-        </div>
-    );
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/users")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header></Header>
+                <main className="c-HomePage-main">
+                    <p>{this.state.apiResponse}</p>
+                    <div className="c-HomePage-main__content">
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                        <Link className="c-HomePage-main__content-item" to="#"></Link>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default HomePage;
